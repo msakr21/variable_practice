0305094bed3c602e512f3d9d3d73c2f78cc96aaf
Mostafa Sakr <msakr3@gmail.com>
2022-08-02 18:19:12 -0600

    Add 3 floatato variables

--- variables.rb
+++ variables.rb
@@ -5,3 +5,7 @@ roommate = "trenton"
 cups_of_coffee_drunk = 3
 highest_count_on_fingers = 9
 organization_member = 14
+
+number_of_funny_friends = 4.5
+twelve_over_five = 2.4
+brain_cells_working = 1.5
